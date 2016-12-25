ee204b33c9b846f6ac19ae676fbf6626b8417950
dumconstantin <dum.constantin@gmail.com>
2016-12-25 16:04:18 +0200

    Temp

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+// Load all html files
+// Load the data.yml
+// Use the default one (app.html) to start processing
+
+// Load data.yml
+// Assess which data corresponds to which element
+// parse each path and uncover what props each
+// component is waiting for
+
+// Create Vue components for each element
+// from app onwards
+
+// Mount app.html
+// And on mount for every component it encounters
+// bind to the data stream
+
+
