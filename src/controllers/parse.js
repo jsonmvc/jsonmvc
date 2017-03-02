78f28c0a0f2350815c91184f5be5d69192958a75
dumconstantin <dum.constantin@gmail.com>
2017-03-02 17:43:14 +0200

    Add parsing file and documentation. Other improvements.

---
+++ src/controllers/parse.js
@@ -0,0 +1,34 @@
+
+
+/**
+ * Reads and validates the following syntax:
+ *
+ * module.exports = {
+ *  args: Object | String | Array,
+ *  stream: Function,
+ *  patch: x => ({
+ *    op: 'add',
+ *    path: '/foo/bar2',
+ *    value: x
+ *  })
+ * }
+ *
+ * args
+ * -- Object: { title: '/foo/bar', date: '/time/ms'}}
+ * -- String: '/foo/bar'
+ * -- Array: ['/foo/bar', '/time']
+ *
+ * stream: Function
+ * function (stream) {
+ *    return stream
+ *      .map(x => x + 1)
+ *  }
+ */
+
+function parse(controller) {
+
+}
+
+module.exports = parse
+
+
---
+++ src/models/parse.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * Reads and validates the following syntax:
+ *
+ * module.exports = {
+ *  path: String,
+ *  args: Object | String | Array,
+ *  fn: Function
+ * }
+ */
+
+function parse(model) {
+
+}
+
+module.exports = parse
