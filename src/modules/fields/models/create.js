687cfb7ccd885332fd59f90284b3f606a7978c8b
dumconstantin <dum.constantin@gmail.com>
2017-04-04 11:02:11 +0300

    Add dynamicOptions support for choice form types

--- src/modules/fields/models/create.js
+++ src/modules/fields/models/create.js
@@ -3,12 +3,18 @@ import reduce from 'lodash/reduce'
 
 module.exports = {
   path: '/fields/create',
-  args: ['/fields/data', '/fields/templates'],
-  fn: (data, templates) => {
+  args: ['/fields/data', '/fields/templates', '/fields/options'],
+  fn: (data, templates, options) => {
 
     return transform(templates, (acc, val, key) => {
       acc[key] = transform(val, (acc2, val2, key2) => {
         acc2[key2] = data[key2]
+
+        if (acc2[key2].dynamicOptions && options) {
+          acc2[key2].options = options[acc2[key2].dynamicOptions]
+          delete acc2[key2].dynamicOptions
+        }
+
       })
     })
 
