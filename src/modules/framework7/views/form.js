de882842f48cf2b88688cf767e4c0a4a0bbdf9a9
dumconstantin <dum.constantin@gmail.com>
2017-05-07 15:55:56 +0300

    Add selected attribute for radio f7form.

--- src/modules/framework7/views/form.js
+++ src/modules/framework7/views/form.js
@@ -67,7 +67,7 @@ module.exports = {
       div(v-if="field.type === 'choice' && field.style === 'radio'")
         .content-title {{ field.label[lang] }}
         ul
-          li(v-for="(value, key) in field.options")
+          li(v-for="(value, key) in field.options", :selected="field.value === key")
             label.item-content.label-radio
               input(type="radio", :name="field.key", :value="key", :data-path="path + '/' + field.key", :data-value="key")
               .item-inner
