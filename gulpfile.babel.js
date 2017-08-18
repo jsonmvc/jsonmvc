a053a37ea302706ff4565f02b37422d361be8efe
dumconstantin <dum.constantin@gmail.com>
2017-08-18 03:08:20 +0300

    chore: Add central build command.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp'
+import glob from 'glob'
 import del from 'del'
 import { lstatSync, readdirSync } from 'fs'
 import path from 'path'
+import now from 'performance-now'
 
 const argv = require('yargs').argv
 
@@ -11,19 +13,45 @@ const rollupConfig = require('./rollup.config.js')
 const base = path.join(__dirname, '..', 'packages')
 const scripts = "./../packages"
 
+
+const forEachPackage = cb => {
+  glob(__dirname + '/packages/*', (err, folders) => {
+    let len = folders.length
+    folders.forEach(x => {
+      let folder = path.parse(x).name
+      cb(folder, len)
+    })
+  })
+}
+
 gulp.task('clean', () => {
   del.sync([ __dirname + '/packages/' + argv.package + '/dist' ])
   del.sync([ __dirname + '/packages/' + argv.package + '/coverage' ])
 })
 
-gulp.task('build', () => {
+gulp.task('build', done => {
+  let finished = 0
 
-  let config = rollupConfig({
-    package: argv.package
-  })
-
-  return rollup.rollup(config)
-    .then(bundle => {
-       bundle.write(config)
+  forEachPackage((x, count) => {
+    let config = rollupConfig({
+      package: x
     })
+
+    let start = now()
+
+    rollup
+      .rollup(config)
+      .then(bundle => {
+         bundle.write(config)
+
+         let stop = now()
+         let dur = ((stop-start) / 1000).toFixed(2)
+
+         console.log(`[ ${dur}s ] ${x}`)
+         finished += 1
+         if (finished === count) {
+          done()
+         }
+      })
+  })
 })
--- packages/jsonmvc-helper-guid/test/index.js
+++ packages/jsonmvc-helper-guid/test/index.js
@@ -1,4 +1,8 @@
 
-it('should work', () => {
-  expect(0).toBe(0)
+import guid from './../src/index'
+
+it('should return a random 6 chars string', () => {
+  let val = guid()
+  expect(val).toMatch(/^[0-9a-z]{6}$/)
+  expect(val).not.toBe(guid())
 })
