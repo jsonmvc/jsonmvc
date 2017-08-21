4444a847382b5983a991613d1d546e861a5a4720
dumconstantin <dum.constantin@gmail.com>
2017-08-22 01:24:44 +0300

    fix(jsonmvc): Remove extra param for module indexing.

--- packages/jsonmvc/src/fns/bundleModules.js
+++ packages/jsonmvc/src/fns/bundleModules.js
@@ -3,11 +3,11 @@ import reduce from 'lodash-es/reduce'
 import merge from 'lodash-es/merge'
 
 function bundleModules (modules) {
-  let bundle = reduce(modules, (acc, v1, k1) => {
+  let bundle = reduce(modules, (acc, v1) => {
     reduce(v1, (acc2, v2, k2) => {
       if (k2 !== 'data') {
         v2 = reduce(v2, (acc3, v3, k3) => {
-          acc3[k1 + '/' + k3] = v3
+          acc3[k3] = v3
           return acc3
         }, {})
       }
