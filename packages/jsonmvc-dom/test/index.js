6861f9194bd8f9da767f2b3cc361bbcb0fc73898
dumconstantin <dum.constantin@gmail.com>
2018-12-20 03:51:43 +0200

    Initial commit.

---
+++ packages/jsonmvc-dom/test/index.js
@@ -0,0 +1,16 @@
+let lib
+if(__DEV__) {
+  lib = require('./../src/index').default
+} else {
+  lib = require('./../dist/jsonmvc-dom')
+}
+
+const { Document } = require('nodom');
+
+jest.useFakeTimers()
+
+it('should test basic parsing', () => {
+
+  let result = lib()
+
+})
