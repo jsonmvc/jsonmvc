1a222b5a3bde6bb8bf2e5cf238f717db918e1ce4
dumconstantin <dum.constantin@gmail.com>
2019-01-17 13:41:14 +0200

    Remove extra files.

--- packages/dom/src/lexer.js
+++ packages/dom/src/lexer.js
@@ -1,19 +1,13 @@
-const TokenStream = require("./compiler/tokenStream");
-const InputStream = require("./compiler/inputStream");
+const TokenStream = require('./compiler/tokenStream');
+const InputStream = require('./compiler/inputStream');
 
 let template = `
 div
-  | .foobar
+  |.foobar
   |#f // both are valid with or without whitespace
-  |(
-  | data-patch:mouseenter="
-  |   add /foo/bar 123;
-  |   add /bar/baz 321;
-  | "
-  | data-patch:click="
-  |   add /foo/bar 123;
-  | "
-  |) foo bar baz bam {{ foo }}
+  |[add /foo/bar 123]
+  |[mouseenter: add /bam/boo {{ foo }}]
+  | foo bar baz bam {{ foo }}
 `;
 
 let inputStream = new InputStream(template);
